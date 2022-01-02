73096a2d81498e4182a4b78ce6429e69b110186f
Andrey <82231196+Badebum@users.noreply.github.com>
2022-01-02 16:33:50 +0300

    reviews page

--- js/menu.js
+++ js/menu.js
@@ -2,13 +2,22 @@
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
   const backDropMenu = document.querySelector('[data-back-drop]');
-  const closeBtn = document.querySelector('[data-menu-close]');
+  const closeBtnRef = document.querySelector('[data-menu-close]');
 
   menuBtnRef.addEventListener('click', () => {
     const expanded =
       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
-    closeBtn.setAttribute('aria-expanded', !expanded);
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('is-open');
+    menuBtnRef.classList.toggle('is-open');
+    backDropMenu.classList.toggle('is-open');
+  });
+
+  closeBtnRef.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
     menuBtnRef.setAttribute('aria-expanded', !expanded);
 
