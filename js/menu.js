f3ff2391f83bd090f73f2d2fdebc0599ea88adda
Andrey <82231196+Badebum@users.noreply.github.com>
2022-01-18 16:52:24 +0300

    prod page mobail

--- js/menu.js
+++ js/menu.js
@@ -26,3 +26,32 @@
     backDropMenu.classList.toggle('is-open');
   });
 })();
+
+(() => {
+  const menuBtnRef = document.querySelector('[data-menu-prod-button]');
+  const mobileMenuRef = document.querySelector('[data-menu-prod]');
+  const backDropMenu = document.querySelector('[data-back-drop-prod]');
+  const closeBtnRef = document.querySelector('[data-menu-close-prod]');
+
+  menuBtnRef.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('is-open');
+    menuBtnRef.classList.toggle('is-open');
+    backDropMenu.classList.toggle('is-open');
+  });
+
+  closeBtnRef.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('is-open');
+    menuBtnRef.classList.toggle('is-open');
+    backDropMenu.classList.toggle('is-open');
+  });
+})();
